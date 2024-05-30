570840869fe62be83bb7913a22a40d62dd57c335
luisc <luisc@LUIS>
2024-05-29 20:03:48 -0500

    Agregar archivos de proyecto.

---
+++ ProyectoBiblioteca/Logica/Conexion.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoBiblioteca.Logica
+{
+    public class Conexion
+    {
+        public static string CN = "Data Source=LUIS\\SQLEXPRESS;Initial Catalog=DB_BIBLIOTECA;Integrated Security=True;TrustServerCertificate=True";
+    }
+}
